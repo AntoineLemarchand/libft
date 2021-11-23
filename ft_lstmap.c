cb4791df91074d94494fe1bb47db4ea263066fcd
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2021-11-23 15:28:21 +0100

    optimized lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 12:10:57 by alemarch          #+#    #+#             */
-/*   Updated: 2021/11/23 12:18:27 by alemarch         ###   ########.fr       */
+/*   Updated: 2021/11/23 15:27:58 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*ret;
 	int		i;
 
-	i = 0;
 	if (!lst || !f || !del)
 		return (NULL);
 	ret = ft_lstnew(f(lst->content));
 	lst = lst->next;
-	i++;
+	i = 1;
 	if (!ret)
 	{
 		free(ret);
 		return (NULL);
 	}
-	lst = lst->next;
 	while (lst)
 	{
 		ft_lstadd_back(&ret, ft_lstnew(f(lst->content)));
-		if (ft_lstsize(ret) != i++)
+		if (ft_lstsize(ret) != ++i)
+		{
 			ft_lstclear(&ret, del);
+			return (NULL);
+		}
 		lst = lst->next;
 	}
 	return (ret);
