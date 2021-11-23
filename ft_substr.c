f3ac66276fe52fff007abe69175346995c87a4eb
Antoine LEMARCHAND <alemarch@e2r7p3.clusters.42paris.fr>
2021-11-23 09:50:59 +0100

    fixed substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 15:34:49 by alemarch          #+#    #+#             */
-/*   Updated: 2021/11/22 15:18:51 by alemarch         ###   ########.fr       */
+/*   Updated: 2021/11/23 09:49:15 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	ret = malloc((len + 1) * sizeof(char));
 	if (!ret || !s)
 		return (NULL);
-	while (i < len)
+	else if (start > ft_strlen((char *)s))
+		return ("");
+	while (i < len && i < ft_strlen((char *)s))
 		ret[i++] = s[start++];
 	ret[i] = '\0';
 	return (ret);
